c8020b744bc483212c6055f6de9b3ccb7bda7186
Valery Dubrava <valery.dubrava@gmail.com>
2017-08-17 23:22:40 +0300

    introduce perister; run get block in multiple threads

--- txexport/src/main/java/io/lastwill/Exporter.java
+++ txexport/src/main/java/io/lastwill/Exporter.java
@@ -14,29 +14,36 @@ import org.web3j.protocol.core.methods.response.EthBlock;
 import org.web3j.protocol.http.HttpService;
 
 import javax.annotation.PostConstruct;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @Component
 public class Exporter implements ApplicationRunner {
-
     @Value("${io.lastwill.jdbc-url}")
     private String JDBC_URL;
+
     @Value("${io.lastwill.web3-url}")
     private String WEB3_URL;
+
+    @Value("${io.lastwill.exporter.pool-size:10}")
+    private int poolSize;
 
     @Autowired
     private CloseableHttpClient closeableHttpClient;
 
+    @Autowired
+    private Persister persister;
+
     private final AtomicBoolean terminated = new AtomicBoolean(false);
     private final Object waitExit = new Object();
+    private Semaphore semaphore;
+    private final Queue<EthBlock> resultQueue = new ConcurrentLinkedQueue<>();
 
     @PostConstruct
     protected void init() {
@@ -53,14 +60,7 @@ public class Exporter implements ApplicationRunner {
             }
         }));
         log.info("Shutdown hook deployed.");
-    }
-
-    private static BigDecimal toEther(BigInteger amount) {
-        if (amount == null) {
-            return new BigDecimal(0L);
-        }
-        return new BigDecimal(amount)
-                .divide(BigDecimal.valueOf(1000000000000000000L), 18, RoundingMode.CEILING);
+        semaphore = new Semaphore(poolSize);
     }
 
     @Override
@@ -99,109 +99,55 @@ public class Exporter implements ApplicationRunner {
             return;
         }
 
-        @Cleanup
-        PreparedStatement insertTransaction = connection.prepareStatement("INSERT INTO transaction (id, block_number, value, from_address, to_address, nonce, gas, gas_price) VALUES (decode(?, 'hex'), ?, ?, decode(?, 'hex'), decode(?, 'hex'), ?, ?, ?)");
-        final int ID_ORDINAL = 1;
-        final int BLOCK_NUMBER_ORDINAL = 2;
-        final int VALUE_ORDINAL = 3;
-        final int FROM_ORDINAL = 4;
-        final int TO_ORDINAL = 5;
-        final int NONCE_ORDINAL = 6;
-        final int GAS_ORDINAL = 7;
-        final int GAS_PRICE_ORDINAL = 8;
-
-        @Cleanup
-        PreparedStatement insertBlock = connection.prepareStatement("INSERT INTO block (id, timestamp, miner_address, difficulty) VALUES (?, ?, decode(?, 'hex'), ?)");
-        final int TIMESTAMP_ORDINAL = 2;
-        final int MINER_ORDINAL = 3;
-        final int DIFFICULTY_ORDINAL = 4;
-
         long transactionIndex = 0;
         long blockNumber = minBlockNumber;
         for (; blockNumber <= currentBlockNumber; blockNumber++) {
-            EthBlock.Block block = web3j.ethGetBlockByNumber(new DefaultBlockParameterNumber(blockNumber), true)
-                    .send()
-                    .getBlock();
 
-            if (block == null) {
-                log.warn("Skip block {}, null returned.", blockNumber);
-                continue;
+            if (!terminated.get()) {
+                semaphore.acquire();
+                final long fBlockNumber = blockNumber;
+                web3j.ethGetBlockByNumber(new DefaultBlockParameterNumber(blockNumber), true)
+                        .sendAsync()
+                        .thenAccept(block -> {
+                            if (block == null) {
+                                log.error("Empty block returned");
+                                semaphore.release();
+                                return;
+                            }
+                            block.setId(fBlockNumber);
+                            resultQueue.add(block);
+                            semaphore.release();
+                        });
+            }
+            else {
+                int restRequest = poolSize - semaphore.availablePermits();
+                if (resultQueue.isEmpty() && restRequest == 0) {
+                    break;
+                }
+                log.info("Terminated cycle, do not request more data. Wait till {} requests will be finished and {} block will be persisted.", restRequest, resultQueue.size());
             }
 
-            insertBlock.setLong(ID_ORDINAL, blockNumber);
-            insertBlock.setInt(TIMESTAMP_ORDINAL, block.getTimestamp() == null ? 0 : block.getTimestamp().intValue());
-            insertBlock.setString(MINER_ORDINAL, block.getMiner().substring(2));
-            insertBlock.setLong(DIFFICULTY_ORDINAL, block.getDifficulty().longValue());
-            insertBlock.addBatch();
-
-            for (EthBlock.TransactionResult<EthBlock.TransactionObject> transaction : block.getTransactions()) {
-                EthBlock.TransactionObject transactionObject = transaction.get();
-                try {
-                    String id = transactionObject.getHash();
-                    String from = transactionObject.getFrom();
-                    String to = transactionObject.getTo();
-                    if (to == null) {
-                        to = "0x0000000000000000000000000000000000000000";
-                    }
-
-                    insertTransaction.setString(ID_ORDINAL, id.substring(2));
-                    insertTransaction.setBigDecimal(VALUE_ORDINAL, toEther(transactionObject.getValue()));
-                    insertTransaction.setLong(BLOCK_NUMBER_ORDINAL, blockNumber);
-                    insertTransaction.setString(FROM_ORDINAL, from.substring(2));
-                    insertTransaction.setString(TO_ORDINAL, to.substring(2));
-                    insertTransaction.setLong(NONCE_ORDINAL, transactionObject.getNonce().longValue());
-                    insertTransaction.setBigDecimal(GAS_ORDINAL, toEther(transactionObject.getGas()));
-                    insertTransaction.setBigDecimal(GAS_PRICE_ORDINAL, toEther(transactionObject.getGasPrice()));
-                    insertTransaction.addBatch();
-                    transactionIndex ++;
-                }
-                catch (Exception e) {
-                    log.error("Skip transaction {}.", transactionObject.getHash(), e);
-                }
-            }
-
-            if (transactionIndex % 100 == 0 && transactionIndex != 0) {
-                try {
-                    insertTransaction.executeBatch();
-                }
-                catch (Exception e) {
-                    log.error("Error on saving transaction batch.", e);
-                }
-                log.debug("Saved block {}, transaction {}.", blockNumber, transactionIndex);
-                if (transactionIndex % 10000 == 0 && !log.isDebugEnabled()) {
-                    log.info("Saved block {}, transaction {}.", blockNumber, transactionIndex);
-                }
-            }
-
-            if (blockNumber % 100 == 0 && blockNumber != 0) {
-                try {
-                    insertBlock.executeBatch();
-                }
-                catch (Exception e) {
-                    log.error("Error on saving block batch.", e);
-                }
-                log.debug("Saved block {}, transaction {}.", blockNumber, transactionIndex);
-                if (blockNumber % 1000 == 0 && !log.isDebugEnabled()) {
-                    log.info("Saved block {}, transaction {}.", blockNumber, transactionIndex);
-                }
-            }
-
-            if (terminated.get()) {
-                log.info("Terminated cycle.");
-                break;
+            while (!resultQueue.isEmpty()) {
+                EthBlock ethBlock = resultQueue.remove();
+                persister.persist(ethBlock.getId(), ethBlock.getBlock());
             }
         }
 
-        insertTransaction.executeBatch();
-        insertBlock.executeBatch();
-        log.info("Saved block {}, transaction {}.", blockNumber, transactionIndex);
+        while (!resultQueue.isEmpty()) {
+            EthBlock ethBlock = resultQueue.remove();
+            persister.persist(ethBlock.getId(), ethBlock.getBlock());
+        }
+
+        persister.flushBlock();
+        persister.flushTransaction();
+        persister.close();
 
         synchronized (waitExit) {
             waitExit.notifyAll();
         }
     }
 
-//    public static byte[] addressToBinary(String address) {
+    //    public static byte[] addressToBinary(String address) {
 //        if (address.startsWith("0x")) {
 //            address = address.substring(2);
 //        }
---
+++ txexport/src/main/java/io/lastwill/Persister.java
@@ -0,0 +1,172 @@
+package io.lastwill;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.web3j.protocol.core.methods.response.EthBlock;
+
+import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Slf4j
+@Component
+public class Persister {
+    @Value("${io.lastwill.jdbc-url}")
+    private String JDBC_URL;
+
+    private Connection connection;
+
+    private PreparedStatement insertTransaction;
+    private final int ID_ORDINAL = 1;
+    private final int BLOCK_NUMBER_ORDINAL = 2;
+    private final int VALUE_ORDINAL = 3;
+    private final int FROM_ORDINAL = 4;
+    private final int TO_ORDINAL = 5;
+    private final int NONCE_ORDINAL = 6;
+    private final int GAS_ORDINAL = 7;
+    private final int GAS_PRICE_ORDINAL = 8;
+
+    private PreparedStatement insertBlock;
+    private final int TIMESTAMP_ORDINAL = 2;
+    private final int MINER_ORDINAL = 3;
+    private final int DIFFICULTY_ORDINAL = 4;
+
+    private long transactionIndex;
+
+
+    @PostConstruct
+    protected void init() throws SQLException {
+        connection = DriverManager.getConnection(
+                JDBC_URL,
+                "ether",
+                "ether"
+        );
+        log.info("Connected to database {}.", JDBC_URL);
+
+        insertTransaction = connection.prepareStatement("INSERT INTO transaction (id, block_number, value, from_address, to_address, nonce, gas, gas_price) VALUES (decode(?, 'hex'), ?, ?, decode(?, 'hex'), decode(?, 'hex'), ?, ?, ?)");
+        insertBlock = connection.prepareStatement("INSERT INTO block (id, timestamp, miner_address, difficulty) VALUES (?, ?, decode(?, 'hex'), ?)");
+    }
+
+    public void persist(long blockNumber, EthBlock.Block block) {
+        try {
+            innerPersist(blockNumber, block);
+        }
+        catch (SQLException e) {
+            log.error("Error on adding to batch block {}, skip it.", blockNumber, e);
+        }
+
+        if (blockNumber % 100 == 0 && blockNumber != 0) {
+            flushBlock();
+            log.debug("Flushed {} blocks.", blockNumber);
+            if (blockNumber % 1000 == 0 && ! log.isDebugEnabled()) {
+                log.info("Flushed {} blocks.", blockNumber);
+            }
+        }
+
+        if (transactionIndex % 1000 == 0 && transactionIndex != 0) {
+            flushTransaction();
+            log.debug("Flushed {} transactions.", transactionIndex);
+            if (transactionIndex % 1000 == 0 && ! log.isDebugEnabled()) {
+                log.info("Flushed {} transactions.", transactionIndex);
+            }
+        }
+    }
+
+    public void flushBlock() {
+        try {
+            insertBlock.executeBatch();
+        }
+        catch (SQLException e) {
+            log.error("Error on saving block batch.", e);
+        }
+    }
+
+    public void flushTransaction() {
+        try {
+            insertTransaction.executeBatch();
+        }
+        catch (SQLException e) {
+            log.error("Error on savging transaction batch.", e);
+        }
+    }
+
+    private void innerPersist(long blockNumber, EthBlock.Block block) throws SQLException {
+        insertBlock.setLong(ID_ORDINAL, blockNumber);
+        insertBlock.setInt(TIMESTAMP_ORDINAL, block.getTimestamp() == null ? 0 : block.getTimestamp().intValue());
+        insertBlock.setString(MINER_ORDINAL, block.getMiner().substring(2));
+        insertBlock.setLong(DIFFICULTY_ORDINAL, block.getDifficulty().longValue());
+        insertBlock.addBatch();
+
+        for (EthBlock.TransactionResult<EthBlock.TransactionObject> transaction : block.getTransactions()) {
+            EthBlock.TransactionObject transactionObject = transaction.get();
+            try {
+                String id = transactionObject.getHash();
+                String from = transactionObject.getFrom();
+                String to = transactionObject.getTo();
+                if (to == null) {
+                    to = "0x0000000000000000000000000000000000000000";
+                }
+
+                insertTransaction.setString(ID_ORDINAL, id.substring(2));
+                insertTransaction.setBigDecimal(VALUE_ORDINAL, toEther(transactionObject.getValue()));
+                insertTransaction.setLong(BLOCK_NUMBER_ORDINAL, blockNumber);
+                insertTransaction.setString(FROM_ORDINAL, from.substring(2));
+                insertTransaction.setString(TO_ORDINAL, to.substring(2));
+                insertTransaction.setLong(NONCE_ORDINAL, transactionObject.getNonce().longValue());
+                insertTransaction.setBigDecimal(GAS_ORDINAL, toEther(transactionObject.getGas()));
+                insertTransaction.setBigDecimal(GAS_PRICE_ORDINAL, toEther(transactionObject.getGasPrice()));
+                insertTransaction.addBatch();
+                transactionIndex ++;
+            }
+            catch (Exception e) {
+                log.error("Skip transaction {}.", transactionObject.getHash(), e);
+            }
+        }
+    }
+
+    private static BigDecimal toEther(BigInteger amount) {
+        if (amount == null) {
+            return new BigDecimal(0L);
+        }
+        return new BigDecimal(amount)
+                .divide(BigDecimal.valueOf(1000000000000000000L), 18, RoundingMode.CEILING);
+    }
+
+//    @Override
+    public void close() throws Exception {
+        if (insertTransaction != null) {
+            flushTransaction();
+            insertTransaction.close();
+        }
+        if (insertBlock != null) {
+            flushBlock();
+            insertBlock.close();
+        }
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+
+//    private ExecutorService executorService;
+//
+//    @Value("${io.lastwill.persister.pool-size:1}")
+//    private int poolSize;
+//
+//    protected void init() {
+//        executorService = Executors.newFixedThreadPool(poolSize);
+//        executorService.
+//    }
+//
+//    public void add(EthBlock.Block block) {
+//
+//    }
+//
+//    private class BlockSaver
+}

d3b9b5b0c25de9243d3c4d4cd6f04b1daf153479
Valery Dubrava <valery.dubrava@gmail.com>
2018-04-04 16:41:32 +0300

    add rsk testnet/mainnet

--- scanner/src/main/java/io/mywish/scanner/model/NetworkType.java
+++ scanner/src/main/java/io/mywish/scanner/model/NetworkType.java
@@ -3,7 +3,11 @@ package io.mywish.scanner.model;
 public enum NetworkType {
     ETHEREUM_MAINNET,
     ETHEREUM_ROPSTEN,
+    RSK_MAINNET,
+    RSK_TESTNET,
     ;
-    public final static String ETHEREUM_MAINNET_VALUE = "ETHEREUM_MAINNET";
-    public final static String ETHEREUM_ROPSTEN_VALUE = "ETHEREUM_ROPSTEN";
+    public final static String ETHEREUM_MAINNET_VALUE = ETHEREUM_MAINNET.name();
+    public final static String ETHEREUM_ROPSTEN_VALUE = ETHEREUM_ROPSTEN.name();
+    public final static String RSK_MAINNET_VALUE = RSK_MAINNET.name();
+    public final static String RSK_TESTNET_VALUE = RSK_TESTNET.name();
 }

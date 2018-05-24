57fafa6ac80f464a6d68660abfe5a94e53c1fbcd
kirillsurkov <surkov.k@mywish.io>
2018-05-24 18:23:58 +0300

    filter null logs

--- wrapper/src/main/java/io/mywish/wrapper/transaction/receipt/WrapperTransactionReceiptWeb3.java
+++ wrapper/src/main/java/io/mywish/wrapper/transaction/receipt/WrapperTransactionReceiptWeb3.java
@@ -8,6 +8,7 @@ import org.web3j.utils.Numeric;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class WrapperTransactionReceiptWeb3 extends WrapperTransactionReceipt {
@@ -17,7 +18,13 @@ public class WrapperTransactionReceiptWeb3 extends WrapperTransactionReceipt {
                 new ArrayList<String>() {{
                     add(web3TxReceipt.getContractAddress());
                 }},
-                web3TxReceipt.getLogs().stream().map(log -> new WrapperLogWeb3(log, eventDefinitionsBySignature.get(log.getTopics().get(0)))).collect(Collectors.toList()),
+                web3TxReceipt.getLogs().stream().map(log -> {
+                    String signature = log.getTopics().get(0);
+                    if (eventDefinitionsBySignature.containsKey(signature)) {
+                        return new WrapperLogWeb3(log, eventDefinitionsBySignature.get(signature));
+                    }
+                    return null;
+                }).filter(Objects::nonNull).collect(Collectors.toList()),
                 (web3TxReceipt.getStatus().startsWith("0x") ? Numeric.decodeQuantity(web3TxReceipt.getStatus()) : new BigInteger(web3TxReceipt.getStatus()))
                         .compareTo(BigInteger.ZERO) != 0
         );

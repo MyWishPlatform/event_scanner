78d5ae121cad6cc6f475312b5448f786837aba0c
kirillsurkov <surkov.k@mywish.io>
2018-05-24 16:28:36 +0300

    some fixes

--- joule/src/main/java/io/mywish/joule/service/JouleService.java
+++ joule/src/main/java/io/mywish/joule/service/JouleService.java
@@ -3,8 +3,8 @@ package io.mywish.joule.service;
 import io.mywish.joule.contracts.JouleAPI;
 import io.mywish.joule.model.JouleRegistrationState;
 import io.mywish.joule.repositories.JouleRegistrationRepository;
-import io.mywish.scanner.WrapperTransaction;
-import io.mywish.scanner.WrapperTransactionReceipt;
+import io.mywish.wrapper.WrapperTransaction;
+import io.mywish.wrapper.WrapperTransactionReceipt;
 import io.mywish.scanner.model.NewBlockEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
 import java.io.IOException;
 import java.util.List;
@@ -41,15 +42,15 @@ public class JouleService {
         }
 
         for (WrapperTransaction transaction: transactions) {
-            if (transaction.getTo() == null) {
+            if (transaction.getOutputs().get(0) == null) {
                 continue;
             }
 
-            if (!jouleAddress.equalsIgnoreCase(transaction.getTo().toLowerCase())) {
+            if (!jouleAddress.equalsIgnoreCase(transaction.getOutputs().get(0).getAddress().toLowerCase())) {
                 continue;
             }
 
-            WrapperTransactionReceipt receipt;
+            TransactionReceipt receipt;
             try {
                 receipt = web3j.ethGetTransactionReceipt(transaction.getHash())
                         .send()

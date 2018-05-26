65193484e59a8e45f687e8efba12b850a10649cb
kirillsurkov <surkov.k@mywish.io>
2018-05-26 16:26:53 +0300

    add mint event and map neo event names to actual event names

--- wrapper/src/main/java/io/mywish/wrapper/service/log/WrapperLogNeoService.java
+++ wrapper/src/main/java/io/mywish/wrapper/service/log/WrapperLogNeoService.java
@@ -6,13 +6,26 @@ import io.mywish.wrapper.WrapperLog;
 import io.mywish.wrapper.WrapperType;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class WrapperLogNeoService {
+    private Map<String, String> ethNametoNeoName = new HashMap<String, String>() {{
+        put("transfer", "Transfer");
+        put("mint", "Mint");
+        put("finishMint", "MintFinished");
+        put("init", "Initialized");
+        put("transferOwnership", "OwnershipTransferred");
+    }};
+
     public WrapperLog build(Event event, ContractEventDefinition definition) {
+        String name = ethNametoNeoName.get(event.getName());
+        if (name == null) {
+            return null;
+        }
         String contract = event.getContract();
-        String name = event.getName();
         List<Object> args = WrapperType.argsFromString(event.getArguments(), definition.getTypes());
         return new WrapperLog(contract, name, args);
     }

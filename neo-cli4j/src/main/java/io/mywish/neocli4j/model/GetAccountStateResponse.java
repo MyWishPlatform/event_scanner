624c6427432efc4785088d657f6e7e1516ce4c23
kirillsurkov <surkov.k@mywish.io>
2018-05-30 18:58:32 +0300

    remastering GetAccountStateResponse class

--- neo-cli4j/src/main/java/io/mywish/neocli4j/model/GetAccountStateResponse.java
+++ neo-cli4j/src/main/java/io/mywish/neocli4j/model/GetAccountStateResponse.java
@@ -2,10 +2,6 @@ package io.mywish.neocli4j.model;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
-public class GetAccountStateResponse {
-    private List<Balance> balances = new ArrayList<>();
+public class GetAccountStateResponse extends JsonRpcResponse<AccountState> {
 }

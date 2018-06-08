04420d5e38cfec29f379ef6325fce53ea7ff4b95
Valery Dubrava <valery.dubrava@gmail.com>
2018-06-08 19:23:35 +0300

    add neo crowdsale product

---
+++ mywish-model/src/main/java/io/lastwill/eventscan/model/ProductCrowdsaleNeo.java
@@ -0,0 +1,39 @@
+package io.lastwill.eventscan.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.time.Instant;
+
+@Entity
+@Table(name = "contracts_contractdetailsneoico")
+@PrimaryKeyJoinColumn(name = "contract_id")
+@DiscriminatorValue("7")
+@Getter
+public class ProductCrowdsaleNeo extends Product implements ProductSingleCheck {
+    @Column(name = "stop_date")
+    private int finishTimestamp;
+    @ManyToOne
+    @JoinColumn(name = "neo_contract_crowdsale_id")
+    private Contract crowdsaleContract;
+//    @ManyToOne
+//    @JoinColumn(name = "eth_contract_token_id")
+//    private Contract tokenContract;
+
+    @Override
+    public int getContractType() {
+        return 7;
+    }
+
+    @Override
+    public BigInteger getCheckGasLimit() {
+        // really it is finish gas price
+        return BigInteger.valueOf(200000);
+    }
+
+    @Override
+    public Instant getCheckDate() {
+        return Instant.ofEpochSecond(finishTimestamp);
+    }
+}

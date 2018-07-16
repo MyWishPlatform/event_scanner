41ec704da2ebec541a9a9ba6f9d73adcf83cb24b
Valery Dubrava <valery.dubrava@gmail.com>
2018-07-16 13:19:16 +0300

    bind neo and eth events by signature (instead of name)

--- wrapper/src/main/java/io/mywish/wrapper/service/log/WrapperLogNeoService.java
+++ wrapper/src/main/java/io/mywish/wrapper/service/log/WrapperLogNeoService.java
@@ -25,27 +25,27 @@ public class WrapperLogNeoService {
     private List<ContractEventBuilder<?>> builders = new ArrayList<>();
     private Map<String, ContractEventBuilder<?>> buildersByName = new HashMap<>();
 
-    private final static Map<String, String> ethToNeoNames = new HashMap<String, String>() {{
-        put("Transfer", "transfer");
-        put("Mint", "mint");
-        put("MintFinished", "finishMint");
-        put("Initialized", "init");
-        put("OwnershipTransferred", "transferOwnership");
+    private final static Map<String, String> signatureToNeoNames = new HashMap<String, String>() {{
+        put("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", "transfer");
+        put("0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885", "mint");
+        put("0xae5184fba832cb2b1f702aca6117b8d265eaf03ad33eb133f19dde0f5920fa08", "finishMint");
+        put("0x5daa87a0e9463431830481fd4b6e3403442dfb9a12b9c07597e9f61d50b633c8", "init");
+        put("0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0", "transferOwnership");
     }};
 
     @PostConstruct
     protected void init() throws Exception {
         for (ContractEventBuilder<?> eventBuilder: builders) {
-            String name = eventBuilder.getDefinition().getName();
-            String neoName = ethToNeoNames.get(name);
+            String signature = eventBuilder.getDefinition().getSignature();
+            String neoName = signatureToNeoNames.get(signature);
             if (neoName == null) {
-                log.debug("Builder {} is not supported for Neo.", name);
+                log.debug("Builder {} ({}) is not supported for Neo.", eventBuilder.getClass().getSimpleName(), signature);
                 continue;
             }
             if (buildersByName.containsKey(neoName)) {
                 ContractEventBuilder duplicateBuilder = buildersByName.get(neoName);
                 if (duplicateBuilder.getClass().equals(eventBuilder.getClass())) {
-                    throw new Exception("Duplicate builder " + eventBuilder.getClass() + " with name (" + name + ") " + neoName + " , skip it.");
+                    throw new Exception("Duplicate builder " + eventBuilder.getClass() + " with name (" + signature + ") " + neoName + " , skip it.");
                 }
                 else {
                     log.warn("There two builders with the same name {}: {} and {}. Skip second.", neoName, duplicateBuilder.getClass().getSimpleName(), eventBuilder.getClass().getSimpleName());

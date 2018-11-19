68321a8ad44fd326fc2229456f18a5da05ff1068
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-11-19 20:01:45 +0300

    add DB LastBlockPersister. using it for BTC on condition

---
+++ scanner/src/main/java/io/mywish/scanner/services/LastBlockDbPersister.java
@@ -0,0 +1,49 @@
+package io.mywish.scanner.services;
+
+import io.lastwill.eventscan.model.NetworkType;
+import io.lastwill.eventscan.repositories.LastBlockRepository;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LastBlockDbPersister implements LastBlockPersister {
+    private final NetworkType networkType;
+    private final LastBlockRepository lastBlockRepository;
+    private Long lastBlockNumber;
+
+    public LastBlockDbPersister(@NonNull NetworkType networkType, @NonNull LastBlockRepository lastBlockRepository, Long lastBlock) {
+        this.networkType = networkType;
+        this.lastBlockRepository = lastBlockRepository;
+        this.lastBlockNumber = lastBlock;
+    }
+
+    @Override
+    public void open() {
+        if (lastBlockNumber == null) {
+            try {
+                lastBlockNumber = lastBlockRepository.getLastBlockForNetwork(networkType);
+            } catch (Throwable e) {
+                log.warn("Impossible to read last block from database.", e);
+            }
+        }
+
+        if (lastBlockNumber != null) {
+            saveLastBlock(lastBlockNumber);
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public Long getLastBlock() {
+        return lastBlockNumber;
+    }
+
+    @Override
+    public void saveLastBlock(long blockNumber) {
+        lastBlockNumber = blockNumber;
+        lastBlockRepository.updateLastBlock(networkType, blockNumber);
+    }
+}

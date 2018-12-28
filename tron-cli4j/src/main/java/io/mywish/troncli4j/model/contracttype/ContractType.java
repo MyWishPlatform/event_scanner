29ab38c6617bd5b2f24be56e5653e8108b48f36f
Valery Dubrava <valery.dubrava@gmail.com>
2018-12-28 18:54:35 +0300

    add new contract type

--- tron-cli4j/src/main/java/io/mywish/troncli4j/model/contracttype/ContractType.java
+++ tron-cli4j/src/main/java/io/mywish/troncli4j/model/contracttype/ContractType.java
@@ -39,5 +39,6 @@ public abstract class ContractType {
         ExchangeInjectContract,
         ExchangeWithdrawContract,
         ExchangeTransactionContract,
+        UpdateEnergyLimitContract,
     }
 }

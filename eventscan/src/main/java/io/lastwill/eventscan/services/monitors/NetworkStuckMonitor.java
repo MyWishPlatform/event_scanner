1382d6f632483844c5cbef4eb20ff2d0a6fe684f
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-01-22 18:17:10 +0300

    exponential time to notify about network stuck

--- eventscan/src/main/java/io/lastwill/eventscan/services/monitors/NetworkStuckMonitor.java
+++ eventscan/src/main/java/io/lastwill/eventscan/services/monitors/NetworkStuckMonitor.java
@@ -6,25 +6,37 @@ import io.lastwill.eventscan.model.NetworkType;
 import io.mywish.scanner.model.NewBlockEvent;
 import io.mywish.scanner.model.NewPendingTransactionsEvent;
 import io.mywish.scanner.services.EventPublisher;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Predicate;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class NetworkStuckMonitor {
     private final ConcurrentHashMap<NetworkType, LastEvent> lastBlockEvents = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<NetworkType, LastEvent> lastPendingTxEvents = new ConcurrentHashMap<>();
+
+    private final Map<NetworkType, Long> checkFrequencies = new HashMap<>();
+    private final ConcurrentHashMap<NetworkType, Long> notifyFrequencies = new ConcurrentHashMap<>();
+
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
     @Value("${io.lastwill.eventscan.network-stuck.interval.btc}")
     private long btcInterval;
     @Value("${io.lastwill.eventscan.network-stuck.interval.eth}")
@@ -33,9 +45,38 @@ public class NetworkStuckMonitor {
     private long eosInterval;
     @Value("${io.lastwill.eventscan.network-stuck.interval.pending}")
     private long pendingInterval;
+    @Value("${io.lastwill.eventscan.network-stuck.interval.max-notification}")
+    private long maxFrequency;
 
     @Autowired
     private EventPublisher eventPublisher;
+
+    @PostConstruct
+    protected void init() {
+        checkFrequencies.put(NetworkType.ETHEREUM_MAINNET, ethInterval);
+        checkFrequencies.put(NetworkType.ETHEREUM_ROPSTEN, ethInterval);
+        checkFrequencies.put(NetworkType.BTC_MAINNET, btcInterval);
+        checkFrequencies.put(NetworkType.BTC_TESTNET_3, btcInterval);
+        checkFrequencies.put(NetworkType.RSK_MAINNET, ethInterval);
+        checkFrequencies.put(NetworkType.RSK_TESTNET, ethInterval);
+        checkFrequencies.put(NetworkType.RSK_FEDERATION_MAINNET, ethInterval);
+        checkFrequencies.put(NetworkType.RSK_FEDERATION_TESTNET, ethInterval);
+        checkFrequencies.put(NetworkType.NEO_MAINNET, ethInterval);
+        checkFrequencies.put(NetworkType.NEO_TESTNET, ethInterval);
+        checkFrequencies.put(NetworkType.EOS_MAINNET, eosInterval);
+        checkFrequencies.put(NetworkType.EOS_TESTNET, eosInterval);
+        checkFrequencies.put(NetworkType.TRON_MAINNET, eosInterval);
+        checkFrequencies.put(NetworkType.TRON_TESTNET, eosInterval);
+
+        notifyFrequencies.putAll(checkFrequencies);
+
+        long checkFrequency = checkFrequencies.values()
+                .stream()
+                .min(Long::compareTo)
+                .orElseThrow(NoSuchElementException::new);
+
+        scheduler.scheduleWithFixedDelay(this::checkNetworks, checkFrequency, checkFrequency, TimeUnit.MILLISECONDS);
+    }
 
     @EventListener
     private void newBlockEvent(NewBlockEvent event) {
@@ -69,15 +110,20 @@ public class NetworkStuckMonitor {
         return Collections.unmodifiableMap(this.lastPendingTxEvents);
     }
 
-    private void checkNetwork(Predicate<? super NetworkType> filterCondition, long interval) {
+    protected void checkNetworks() {
         final LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
         lastBlockEvents.keySet()
-                .stream()
-                .filter(filterCondition)
                 .forEach(networkType -> {
                     LastEvent lastEvent = lastBlockEvents.get(networkType);
+                    long checkFrequency = checkFrequencies.get(networkType);
                     // last block + interval is in future
-                    if (lastEvent.receivedTime.plusSeconds(interval / 1000).isAfter(now)) {
+                    if (lastEvent.receivedTime.plusSeconds(checkFrequency / 1000).isAfter(now)) {
+                        notifyFrequencies.put(networkType, checkFrequency);
+                        return;
+                    }
+
+                    long notifyFrequency = notifyFrequencies.get(networkType);
+                    if (lastEvent.receivedTime.plusSeconds(notifyFrequency / 1000).isAfter(now)) {
                         return;
                     }
 
@@ -89,28 +135,12 @@ public class NetworkStuckMonitor {
                                     lastEvent.blockNo
                             )
                     );
+
+                    notifyFrequency = notifyFrequency * 2 < maxFrequency
+                            ? notifyFrequency * 2
+                            : maxFrequency;
+                    notifyFrequencies.put(networkType, notifyFrequency);
                 });
-    }
-
-    @Scheduled(fixedDelayString = "${io.lastwill.eventscan.network-stuck.interval.eth}", initialDelayString = "${io.lastwill.eventscan.network-stuck.interval.eth}")
-    protected void checkRestNetworks() {
-        checkNetwork(networkType -> networkType != NetworkType.BTC_MAINNET
-                        && networkType != NetworkType.BTC_TESTNET_3
-                        && networkType != NetworkType.EOS_MAINNET
-                        && networkType != NetworkType.EOS_TESTNET,
-                ethInterval);
-    }
-
-    @Scheduled(fixedDelayString = "${io.lastwill.eventscan.network-stuck.interval.eos}", initialDelayString = "${io.lastwill.eventscan.network-stuck.interval.eos}")
-    protected void checkEos() {
-        checkNetwork(networkType -> networkType == NetworkType.EOS_TESTNET || networkType == NetworkType.EOS_MAINNET,
-                eosInterval);
-    }
-
-    @Scheduled(fixedDelayString = "${io.lastwill.eventscan.network-stuck.interval.btc}", initialDelayString = "${io.lastwill.eventscan.network-stuck.interval.btc}")
-    protected void checkBtc() {
-        checkNetwork(networkType -> networkType == NetworkType.BTC_MAINNET || networkType == NetworkType.BTC_TESTNET_3,
-                btcInterval);
     }
 
 //    @Scheduled(fixedDelayString = "${io.lastwill.eventscan.network-stuck.interval.pending}", initialDelayString = "${io.lastwill.eventscan.network-stuck.interval.pending}")
@@ -137,15 +167,10 @@ public class NetworkStuckMonitor {
     }
 
     @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
     public static class LastEvent {
         private final LocalDateTime receivedTime;
         private final Instant timestamp;
         private final long blockNo;
-
-        private LastEvent(LocalDateTime receivedTime, Instant timestamp, long blockNo) {
-            this.receivedTime = receivedTime;
-            this.timestamp = timestamp;
-            this.blockNo = blockNo;
-        }
     }
 }

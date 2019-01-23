fd45a0d6dc9ece0abb93490f3913768ff568c0c7
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-01-23 19:06:39 +0300

    optimize block scanner speed

--- eventscan/src/main/java/io/lastwill/eventscan/services/monitors/NetworkSpeedMonitor.java
+++ eventscan/src/main/java/io/lastwill/eventscan/services/monitors/NetworkSpeedMonitor.java
@@ -10,6 +10,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 @Component
 public class NetworkSpeedMonitor {
@@ -25,11 +28,19 @@ public class NetworkSpeedMonitor {
     @Value("${io.lastwill.eventscan.network-speed.interval}")
     private int interval;
 
+    private Instant startTime;
+
+    @PostConstruct
+    protected void init() {
+        Stream.of(NetworkType.values())
+                .forEach(networkType -> blocks.put(networkType, Collections.synchronizedList(new ArrayList<>())));
+        startTime = Instant.now();
+    }
+
     @EventListener
     protected void onNewBlock(NewBlockEvent event) {
         NetworkType networkType = event.getNetworkType();
         Instant now = Instant.now();
-        blocks.putIfAbsent(networkType, Collections.synchronizedList(new ArrayList<>()));
         blocks.computeIfPresent(networkType, (networkType1, blocks) -> {
             blocks.add(new Block(now));
             return blocks;
@@ -53,7 +64,13 @@ public class NetworkSpeedMonitor {
                 .filter(block -> block.receivedTime.plusMillis(interval).isAfter(now))
                 .count();
         long millisInUnit = timeUnit.toMillis(1);
-        return (double) count * millisInUnit / interval;
+
+        long passedTime = Timestamp.from(now).getTime() - Timestamp.from(startTime).getTime();
+        if (passedTime > interval) {
+            passedTime = interval;
+        }
+
+        return (double) count * millisInUnit / passedTime;
     }
 
     @Getter

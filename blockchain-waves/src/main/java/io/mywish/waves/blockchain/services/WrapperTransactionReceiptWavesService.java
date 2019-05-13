61a7bc76fc7444d76d21549a5d834b9c27366716
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-05-13 13:06:32 +0300

    finalized and whitelistAdded events for waves' sto

--- blockchain-waves/src/main/java/io/mywish/waves/blockchain/services/WrapperTransactionReceiptWavesService.java
+++ blockchain-waves/src/main/java/io/mywish/waves/blockchain/services/WrapperTransactionReceiptWavesService.java
@@ -1,16 +1,22 @@
 package io.mywish.waves.blockchain.services;
 
+import io.mywish.blockchain.ContractEvent;
 import io.mywish.blockchain.WrapperOutput;
 import io.mywish.blockchain.WrapperTransactionReceipt;
+import io.mywish.waves.blockchain.model.WrapperOutputWaves;
 import io.mywish.waves.blockchain.model.WrapperTransactionWaves;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class WrapperTransactionReceiptWavesService {
+    @Autowired
+    private WrapperLogWavesService logBuilder;
+
     public WrapperTransactionReceipt build(WrapperTransactionWaves transaction) {
         List<String> contracts = transaction
                 .getOutputs()
@@ -18,10 +24,18 @@ public class WrapperTransactionReceiptWavesService {
                 .map(WrapperOutput::getAddress)
                 .collect(Collectors.toList());
 
+        List<ContractEvent> logs = transaction.getOutputs()
+                .stream()
+                .map(wrapperOutput -> ((WrapperOutputWaves) wrapperOutput))
+                .map(WrapperOutputWaves::getTransaction)
+                .map(logBuilder::build)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
         return new WrapperTransactionReceipt(
                 transaction.getHash(),
                 contracts,
-                Collections.emptyList(),
+                logs,
                 true
         );
     }

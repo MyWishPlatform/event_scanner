35aa974434e542a506f4341798a7758dd48f256b
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-05-23 16:26:10 +0200

    uncomment waves

--- mywish-model/src/main/java/io/lastwill/eventscan/model/ProductStoWaves.java
+++ mywish-model/src/main/java/io/lastwill/eventscan/model/ProductStoWaves.java
@@ -1,6 +1,5 @@
 package io.lastwill.eventscan.model;
 
-/*
 import lombok.Getter;
 
 import javax.persistence.*;
@@ -11,9 +10,7 @@ import java.math.BigInteger;
 @PrimaryKeyJoinColumn(name = "contract_id")
 @DiscriminatorValue("22")
 @Getter
-*/
-public class ProductStoWaves/* extends Product*/ {
-    /*
+public class ProductStoWaves extends Product {
     @Override
     public int getContractType() {
         return 22;
@@ -23,5 +20,4 @@ public class ProductStoWaves/* extends Product*/ {
     public BigInteger getCheckGasLimit() {
         return BigInteger.ZERO;
     }
-     */
 }

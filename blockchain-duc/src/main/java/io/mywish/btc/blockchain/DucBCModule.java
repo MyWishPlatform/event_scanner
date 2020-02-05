afdc63d27dfc5c8d99b77d39d71d5a7bdee28357
Mikhail Rozdin <mrozdin@yandex.ru>
2020-02-05 12:26:15 +0300

    drop testnet

--- blockchain-duc/src/main/java/io/mywish/btc/blockchain/DucBCModule.java
+++ blockchain-duc/src/main/java/io/mywish/btc/blockchain/DucBCModule.java
@@ -10,7 +10,6 @@ import io.mywish.scanner.services.LastBlockFilePersister;
 import io.mywish.scanner.services.LastBlockPersister;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.bitcoinj.params.MainNetParams;
-import org.bitcoinj.params.TestNet3Params;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -24,13 +23,6 @@ import java.net.URI;
 @ComponentScan
 @Configuration
 public class DucBCModule {
-    /**
-     * Solution for test purposes only.
-     * When we scan mainnet blocks we build addresses in mainnet format. And is not the same like testnet address.
-     * This flag is solve the issue.
-     */
-    @Value("${etherscanner.ducatus.treat-testnet-as-mainnet:false}")
-    private boolean treatTestnetAsMainnet;
 
     @ConditionalOnProperty("etherscanner.ducatus.rpc-url.mainnet")
     @Bean(name = NetworkType.DUC_MAINNET_VALUE)
@@ -55,36 +47,8 @@ public class DucBCModule {
                         rpc.getPort(),
                         user,
                         password
-                ),
-                treatTestnetAsMainnet ? new TestNet3Params() : new MainNetParams()
+                ), new MainNetParams()
         );
-    }
-
-    @ConditionalOnProperty("etherscanner.ducatus.rpc-url.testnet")
-    @Bean(name = NetworkType.DUC_TESTNET_VALUE)
-    public DucNetwork ducNetTest(
-            final CloseableHttpClient closeableHttpClient,
-            final @Value("${etherscanner.ducatus.rpc-url.testnet}") URI rpc
-    ) throws Exception {
-        String user = null, password = null;
-        if (rpc.getUserInfo() != null) {
-            String[] credentials = rpc.getUserInfo().split(":");
-            if (credentials.length > 1) {
-                user = credentials[0];
-                password = credentials[1];
-            }
-        }
-        return new DucNetwork(
-                NetworkType.DUC_TESTNET,
-                new BtcdClientImpl(
-                        closeableHttpClient,
-                        rpc.getScheme(),
-                        rpc.getHost(),
-                        rpc.getPort(),
-                        user,
-                        password
-                ),
-                new TestNet3Params());
     }
 
     @Configuration
@@ -96,14 +60,6 @@ public class DucBCModule {
                 final @Value("${etherscanner.ducatus.last-block.mainnet:#{null}}") Long lastBlock
         ) {
             return new LastBlockDbPersister(NetworkType.DUC_MAINNET, lastBlockRepository, lastBlock);
-        }
-
-        @Bean
-        public LastBlockPersister ducTestnetLastBlockPersister(
-                LastBlockRepository lastBlockRepository,
-                final @Value("${etherscanner.ducatus.last-block.testnet:#{null}}") Long lastBlock
-        ) {
-            return new LastBlockDbPersister(NetworkType.DUC_TESTNET, lastBlockRepository, lastBlock);
         }
     }
 
@@ -117,14 +73,6 @@ public class DucBCModule {
         ) {
             return new LastBlockFilePersister(NetworkType.DUC_MAINNET, dir, lastBlock);
         }
-
-        @Bean
-        public LastBlockPersister ducTestnetLastBlockPersister(
-                final @Value("${etherscanner.start-block-dir}") String dir,
-                final @Value("${etherscanner.ducatus.last-block.testnet:#{null}}") Long lastBlock
-        ) {
-            return new LastBlockFilePersister(NetworkType.DUC_TESTNET, dir, lastBlock);
-        }
     }
 
     @ConditionalOnBean(name = NetworkType.DUC_MAINNET_VALUE)
@@ -132,22 +80,6 @@ public class DucBCModule {
     public DucScanner ducScannerMain(
             final @Qualifier(NetworkType.DUC_MAINNET_VALUE) DucNetwork network,
             final @Qualifier("ducMainnetLastBlockPersister") LastBlockPersister lastBlockPersister,
-            final @Value("${etherscanner.ducatus.polling-interval-ms}") Long pollingInterval,
-            final @Value("${etherscanner.ducatus.commit-chain-length}") Integer commitmentChainLength
-    ) {
-        return new DucScanner(
-                network,
-                lastBlockPersister,
-                pollingInterval,
-                commitmentChainLength
-        );
-    }
-
-    @ConditionalOnBean(name = NetworkType.DUC_TESTNET_VALUE)
-    @Bean
-    public DucScanner ducScannerTest(
-            final @Qualifier(NetworkType.DUC_TESTNET_VALUE) DucNetwork network,
-            final @Qualifier("ducTestnetLastBlockPersister") LastBlockPersister lastBlockPersister,
             final @Value("${etherscanner.ducatus.polling-interval-ms}") Long pollingInterval,
             final @Value("${etherscanner.ducatus.commit-chain-length}") Integer commitmentChainLength
     ) {

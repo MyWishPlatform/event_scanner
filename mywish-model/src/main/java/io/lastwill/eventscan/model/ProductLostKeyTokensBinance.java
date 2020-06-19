7b8e427c5780137550224632fbd7297101387aad
Mikhail R <mrozdin@yandex.ru>
2020-06-19 10:31:27 +0300

    lost key token binance product

---
+++ mywish-model/src/main/java/io/lastwill/eventscan/model/ProductLostKeyTokensBinance.java
@@ -0,0 +1,20 @@
+package io.lastwill.eventscan.model;
+
+import lombok.Getter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contracts_contractdetailsbinancelostkeytokens")
+@PrimaryKeyJoinColumn(name = "contract_id")
+@DiscriminatorValue("31")
+@Getter
+public class ProductLostKeyTokensBinance extends ProductLostKeyWallet{
+    @Override
+    public int getContractType() {
+        return 31;
+    }
+}

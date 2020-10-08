6f9b02bfc4733c03e31726b8cd48f7e025d24cf1
ntop <ntop@example.com>
2020-10-08 12:10:59 +0300

    rename rubic to rbc

--- eventscan/src/main/java/io/lastwill/eventscan/services/monitors/payments/ERC20PaymentMonitor.java
+++ eventscan/src/main/java/io/lastwill/eventscan/services/monitors/payments/ERC20PaymentMonitor.java
@@ -5,7 +5,6 @@ import io.lastwill.eventscan.events.model.contract.erc20.TransferEvent;
 import io.lastwill.eventscan.model.CryptoCurrency;
 import io.lastwill.eventscan.model.NetworkType;
 import io.lastwill.eventscan.model.UserSiteBalance;
-import io.lastwill.eventscan.repositories.UserProfileRepository;
 import io.lastwill.eventscan.repositories.UserSiteBalanceRepository;
 import io.lastwill.eventscan.services.TransactionProvider;
 import io.mywish.blockchain.WrapperTransaction;
@@ -53,7 +52,7 @@ public class ERC20PaymentMonitor {
         addressToCurrency.put(tokenAddressWish.toLowerCase(), CryptoCurrency.WISH);
         addressToCurrency.put(tokenAddressBnb.toLowerCase(), CryptoCurrency.BNB);
         addressToCurrency.put(tokenAddressSwap.toLowerCase(), CryptoCurrency.SWAP);
-        addressToCurrency.put(tokenAddressRubic.toLowerCase(), CryptoCurrency.RUBIC);
+        addressToCurrency.put(tokenAddressRubic.toLowerCase(), CryptoCurrency.RBC);
 
         addressToCurrency.forEach(
                 (address, cryptoCurrency) -> log.info("Payment waiting {} - {}.", cryptoCurrency, address)
